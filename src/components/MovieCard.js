e2b27449d9e05c7dcf82ca05225f1730fdaf8691
Tkachenko AI <alexfisher13@yandex.ru>
2018-05-14 17:53:18 +0300

    add new style

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -2,12 +2,13 @@ import React from 'react';
 
 const MovieCard = ({movie}) => {
     return (
-        <div className='finder'>
+        <div className='movie-card'>
+            <img src={movie.Poster}/>
             <div>
-                <h3>Finded movies</h3>
-                <img src={movie.Poster} alt=""/>
                 <p>Название: {movie.Title}</p>
-                <p>Год: {movie.Year}</p>
+                <p>Год: {movie.Released}</p>
+                <p>Production: {movie.Production}</p>
+                <p>Rating: {movie.imdbRating}</p>
             </div>
         </div>
     );
